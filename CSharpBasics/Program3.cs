dec7c74513855fff58c321cf88a40be7e1c21cee
VIJAY KUMAR <76174655+vijayprasin@users.noreply.github.com>
2026-01-26 23:18:07 +0530

    Add checkpassfail method to evaluate pass or fail

---
+++ CSharpBasics/Program3.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program 
+{
+  static void checkpassfail
+  {
+    int marks = 40;
+
+    if (marks >= 35)
+    {
+      Console.WriteLine("pass")
+      }
+    else
+    {
+      Console.WriteLine("fail")
+      }
+  }
+}
